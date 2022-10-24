ac7e8df9761ccdf1f95eeebf482df0e2b91aeffb
chayannesteps <109673257+chayannesteps@users.noreply.github.com>
2022-10-24 14:42:31 -0400

    file add and change

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import {setContext} from 'apollo/client/link/context';
+import {BrowserRouter as Router, Routes}
+
+import Text from './pages/test'
+
+const authLink = setContext((_, {headers}))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    hello
+    <Test />
+    </>
+   
   );
 }
 
