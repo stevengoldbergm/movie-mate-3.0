852a266c30a5490a0ad9d31ccc467a59cb0679bb
SG_Demi <stevengoldbergm@gmail.com>
2022-10-24 13:47:06 -0500

    Set up base for pages

---
+++ client/src/pages/test.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const test = () => {
+    return (
+        <>
+            <h1 className="has-text-centered has-text-weight-bold is-size-1">
+                This is a title test
+            </h1>
+        </>
+    )
+} 
+
+export default test;
