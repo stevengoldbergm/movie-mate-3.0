9f5705f14a7b47fd1bea943f556fcf98d54f8a64
John Tash <jtash25@gmail.com>
2022-10-24 13:54:56 -0400

    Completed models and schema for user, movie and review base functionallity

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,37 @@
-const {  } = require('../models');
+const { Movie, User, Review } = require('../models');
 
 const resolvers = {
   Query: {
-   
+   movie: async () => {
+    return Movie.find({});
+   },
+
+   user: async () => {
+    return User.find({});
+   },
+
+   review: async () => {
+    return Review.find({})
+   }
   },
   Mutation: {
+    createMovie: async (parent, args) => {
+      const movie = await Movie.create(args);
+      return movie;
+    },
    
+    createUser: async (parent ,args) => {
+      const user  = await User.create(args);
+      return user;
+    },
+
+    createReview: async (parent, args) => {
+      const review = await Review.create(args);
+      return review;
+    },
   },
 };
+
+module.exports = resolvers;
 
 module.exports = resolvers;
