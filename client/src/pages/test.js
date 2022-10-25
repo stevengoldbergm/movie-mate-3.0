1a8686665bafab7d818b43cb916df82bc57deeb6
SG_Demi <stevengoldbergm@gmail.com>
2022-10-25 13:29:56 -0500

    test

--- client/src/pages/test.js
+++ client/src/pages/test.js
@@ -3,11 +3,12 @@ import React from "react";
 const test = () => {
     return (
         <>
+            <script src="/axios/dist/axios.min.js"></script>
             <h1 className="has-text-centered has-text-weight-bold is-size-1">
                 This is a title test
             </h1>
         </>
     )
-} 
+}   
 
 export default test;
