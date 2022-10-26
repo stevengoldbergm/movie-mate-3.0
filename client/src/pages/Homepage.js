51304f2740db0d85383719319ee3a0b5a967dfd9
SG_Demi <stevengoldbergm@gmail.com>
2022-10-26 01:52:47 -0500

    Added Searchbar to Homepage

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,28 +1,34 @@
-import React from 'react'
+import React from 'react';
+import SearchBar from '../components/SearchBar';
+
 
 function Homepage() {
   return (
-<div>
-<main className="is-fullheight has-background-white">
-    <div id="search-results" className="is-6 p-4 is-hidden">
-      <div className="card events-card">
-        <header className="card-header ">
-          <p className="card-header-title">
-            Search Results
-          </p>
-        </header>
-        <div className="card-table">
-          <table className="table is-fullwidth is-striped">
-            <tbody id="table-body">
-            </tbody>
-          </table>
-        </div>
+    <>
+      <SearchBar />  
+      <div>
+        <main className="is-fullheight has-background-white">
+            <div id="search-results" className="is-6 p-4 is-hidden">
+              <div className="card events-card">
+                <header className="card-header ">
+                  <p className="card-header-title">
+                    Search Results
+                  </p>
+                </header>
+                <div className="card-table">
+                  <table className="table is-fullwidth is-striped">
+                    <tbody id="table-body">
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </main>
       </div>
-    </div>
-  </main>
-</div>
+    </>
+
 
   )
 }
 
-export default Homepage
+export default Homepage;
