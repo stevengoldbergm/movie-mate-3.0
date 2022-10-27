d078863e1c72e5c1e3f7d6c3b23d347777abf19d
SG_Demi <stevengoldbergm@gmail.com>
2022-10-27 02:58:21 -0500

    Refactored footer. Added links

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,36 +1,32 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <div>
-    <footer className="has-background-white has-text-centered has-text-black py-3 is-bottom">
-  <p>
-    Movie Mate 2.0 © Group 3
-  </p>
-  <div className="hero-foot is-borderless">
-    <nav className="tabs is-centered">
-      <div className="container">
-        <ul>
-          <li>
-            <a><i className="fab fa-github" /></a>
-          </li>
-          <li>
-            <a><i className="fab fa-facebook" /></a>
-          </li>
-          <li>
-            <a><i className="fab fa-instagram" /></a>
-          </li>
-          <li>
-            <a><i className="fab fa-twitter" /></a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </div>
-</footer>
-
-    </div>
-  )
+    <>
+      <footer className="has-background-info has-text-centered has-text-white mt-auto">
+        <p className="mt-5">Movie Mate 2.0 © Group 3</p>
+        <br></br>
+        <div className="hero-foot">
+          <div className="container is-fullwidth">
+            <div className="navbar has-background-info is-flex is-justify-content-center">
+              <a href="https://github.com/" className="mx-4 has-text-white">
+                <i className="fab fa-github" />
+              </a>
+              <a href="https://facebook.com/" className="mx-4 has-text-white">
+                <i className="fab fa-facebook" />
+              </a>
+              <a href="https://instagram.com/" className="mx-4 has-text-white">
+                <i className="fab fa-instagram" />
+              </a>
+              <a href="https://twitter.com/" className="mx-4 has-text-white">
+                <i className="fab fa-twitter" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
