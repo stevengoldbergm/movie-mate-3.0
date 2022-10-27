7560dfdc614edc83545e1b6a490db6878623afc0
chayannesteps <109673257+chayannesteps@users.noreply.github.com>
2022-10-27 11:44:18 -0400

    Landing page creation and navbar/footer update

---
+++ client/src/components/Footer2/Footer2.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Footer.css'
+
+
+function FooterNew() {
+  return (
+    <div className='footer-container'>
+        <section class='social-media'>
+        <div class='social-media-wrap'>
+          <div class='footer-logo'>
+            <Link to='/' className='social-logo'>
+              <i class='fas fa-ticket-alt' />
+              MovieMate
+              <i class='fas fa-ticket-alt' />
+            </Link>
+          </div>
+          <small class='website-rights'>MovieMate © 2022</small>
+          <div class='social-icons'>
+            <Link
+              class='social-icon-link Github'>
+              <i class='fab fa-github' />
+            </Link>
+            <Link
+              class='social-icon-link facebook'>
+              <i class='fab fa-facebook-f' />
+            </Link>
+            <Link
+              class='social-icon-link instagram'>
+              <i class='fab fa-instagram' />
+            </Link>
+            <Link
+              class='social-icon-link twitter'>
+              <i class='fab fa-twitter' />
+            </Link>
+            <Link
+              class='social-icon-link twitter'>
+              <i class='fab fa-linkedin' />
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default FooterNew
