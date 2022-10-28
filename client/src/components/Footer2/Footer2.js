33086a003ac7dd26a268cf1edad0629e83ccfdb1
SG_Demi <stevengoldbergm@gmail.com>
2022-10-28 13:35:29 -0500

    Refactored footer

--- client/src/components/Footer2/Footer2.js
+++ client/src/components/Footer2/Footer2.js
@@ -5,42 +5,63 @@ import './Footer.css'
 
 function Footer2() {
   return (
-    <div className='footer-container'>
-        <section class='social-media'>
-        <div class='social-media-wrap'>
-          <div class='footer-logo'>
-            {/* <Link to='/' className='social-logo'>
+    <footer className='footer-container'>
+      <section class='social-media'>
+        <div class='social-media-wrap is-flex is-flex-direction-column'>
+          {/* <div class='footer-logo'>
+            <Link to='/' className='social-logo'>
               <i class='fas fa-ticket-alt' />
               MOVIEMATE
               <i class='fas fa-ticket-alt' />
-            </Link> */}
-          </div>
+            </Link>
+          </div> */}
           <small class='website-rights'>MovieMate © 2022</small>
-          <div class='social-icons'>
-            <Link
-              class='social-icon-link Github'>
+          <div class='social-icons mb-5'>
+            <a
+              class='social-icon-link Github'
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/"
+            >
               <i class='fab fa-github' />
-            </Link>
-            <Link
-              class='social-icon-link facebook'>
+            </a>
+            <a
+              class='social-icon-link facebook'
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://facebook.com/"
+            >
               <i class='fab fa-facebook-f' />
-            </Link>
-            <Link
-              class='social-icon-link instagram'>
+            </a>
+            <a
+              class='social-icon-link instagram'
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://instagram.com/"
+            >
               <i class='fab fa-instagram' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'>
+            </a>
+            <a
+              class='social-icon-link twitter'
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://twitter.com/"
+            >
               <i class='fab fa-twitter' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'>
+            </a>
+            <a
+              class='social-icon-link linkedin'
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://linkedin.com/"
+              
+            >
               <i class='fab fa-linkedin' />
-            </Link>
+            </a>
           </div>
         </div>
       </section>
-    </div>
+    </footer>
   )
 }
 
