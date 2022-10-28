09c3dd8fcc85915e7aa0650b30924355e44fdc10
SG_Demi <stevengoldbergm@gmail.com>
2022-10-28 07:57:56 -0500

    Added search script to search bar

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,6 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { searchOMDB } from "../utils/API";
 
 function SearchBar() {
+  // Create state for holding our search field data
+  const [searchInput, setSearchInput] = useState("");
+  // Set state object of pulled data
+  const [searchedMovies, setSearchedMovies] = useState([]);
+
+  const handleFormUpdate = (event) => {
+    setSearchInput(event.target.value);
+    console.log(searchInput);
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!searchInput) {
+      return false;
+    }
+
+    console.log(searchInput); // Working
+
+    try {
+      const movies = await searchOMDB(searchInput);
+      console.log("Movies: ", movies);
+      setSearchedMovies(movies);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // Create local storage for movie history
+  // Use context to set a state object of local storage items
+  // Use local storage to generate search history buttons
+  // When you click search history buttons, they search for the movie
+
+  const searchMovies = () => {
+    return;
+  };
+
+  const clearHistory = () => {
+    return;
+  };
+
   return (
     <>
       <header>
@@ -10,9 +53,12 @@ function SearchBar() {
               <header className="columns">
                 <section className="column is-one-fifth is-hidden-mobile" />
                 <section className="column is-three-fifths is-12-mobile">
-                  <form id="srch-form">
+                  <form id="srch-form" onSubmit={handleFormSubmit}>
                     <p className="control center">
                       <input
+                        name="searchInput"
+                        value={searchInput}
+                        onChange={handleFormUpdate}
                         className="input"
                         id="srch-title"
                         type="text"
@@ -59,7 +105,7 @@ function SearchBar() {
                             {/* NOTE: You can't just clear local memory! It will delete the token! */}
                             {/* eslint-disable-next-line */}
                             <a
-                              onclick="clearLocalStorage()"
+                              onClick="clearLocalStorage()"
                               className="dropdown-item"
                             >
                               Clear History
@@ -76,6 +122,51 @@ function SearchBar() {
           </div>
         </section>
       </header>
+
+      {/* Start table object and map out search results */}
+      { searchedMovies.length && (
+        <main className="is-fullheight has-background-white">
+          <div id="search-results" className="is-6 p-4">
+            <div className="card events-card">
+              <header className="card-header ">
+                <p className="card-header-title">Search Results</p>
+              </header>
+              <div className="card-table">
+                <table className="table is-fullwidth is-striped">
+                  <tbody id="table-body">
+                    { searchedMovies.map((movie) => {
+                      console.log(movie.Title, movie.Year, movie.imdbID, )
+                      return (
+                        <tr key={movie.imdbID} className=" is-flex-direction-column is-justify-content-center is-align-items-center">
+                          <td style={{width: "5%"}}>
+                            <i className="fa fa-bell-o is-hidden-mobile"></i>
+                          </td>
+                          <td>
+                            {movie.Title}
+                          </td>
+                          <td>
+                            Year: {movie.Year}
+                          </td>
+                          <td className="level-right">
+                            <Link 
+                              className="button is-small is-info" 
+                              imdb-id={movie.imdbID} 
+                              to={`/movie-details/${ movie.imdbID }`}
+                            >
+                              Movie Details
+                            </Link>
+                          </td>
+                        </tr>
+                      )
+                    })}
+                    
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </main>
+      )}
     </>
   );
 }
