13db0da4e93813437d50c46bcba7550c1c2db3e8
SG_Demi <stevengoldbergm@gmail.com>
2022-10-30 00:21:15 -0500

    Fixed typo

--- client/src/pages/LoginForm.js
+++ client/src/pages/LoginForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations' // Need LOGIN_USER mutation
 import { validateEmail } from '../utils/helpers';
