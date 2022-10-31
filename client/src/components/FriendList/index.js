27e2be26d8177836d9125336f08462ea4166695a
chayannesteps <109673257+chayannesteps@users.noreply.github.com>
2022-10-31 10:15:43 -0400

    nav bar friend list

--- client/src/components/FriendList/index.js
+++ client/src/components/FriendList/index.js
@@ -5,3 +5,29 @@ import './NavBtn.css';
 import { Button } from './NavBtn';
 // import Button from 'react-bootstrap/Button';
 // import Card from 'react-bootstrap/Card';
+
+function FriendList() {
+    return (
+        <>
+        <div className="columns p-4 has-background-white">
+            <div className="is-3 p-3 ">
+        <aside className="menu is-hidden-mobile ">
+        <p className="menu-label">
+            Profile
+        </p>
+            <ul className="menu-list">
+            <Link className="menu-list" to="/Reviews">
+                Your Reviews
+            </Link>
+            <Link className="menu-list" to="/Profile">
+                Your Activity
+            </Link>
+            <Link className="menu-list" to="/Profile">
+                Your Friends
+            </Link>
+            <Link className="menu-list" to="/Profile">
+                Your Invites
+            </Link>
+            </ul>
+        </aside>
+            </div>
