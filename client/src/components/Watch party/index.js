9181a479575f419ee5ffc772334a02f7d7299acc
chayannesteps <109673257+chayannesteps@users.noreply.github.com>
2022-10-31 10:21:05 -0400

    imports watch party list

---
+++ client/src/components/Watch party/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../FriendList.css'
+import '../NavBtn.css';
+import { Button } from '../NavBtn';
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
