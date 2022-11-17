1bd78efc5ca2adfbe7ff2408432e153157bdcb70
SG_Demi <stevengoldbergm@gmail.com>
2022-11-16 18:00:57 -0600

    Integrated custom Bulma SASS file

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import 'bulma/css/bulma.min.css';
+import '../src/assets/css/custom.min.css'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
